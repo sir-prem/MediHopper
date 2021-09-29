f93bded574f7e527115e4f81f47e7b3fb9c750b1
Prem G <sir_prem@hotmail.com>
2021-09-30 03:16:12 +1000

    added functionality to get chat history from a specific user

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,5 +1,24 @@
 const Message = require('../models/message');
 
+// save new messages to DB, and then
+// emit the message to the recipient
+function saveAndEmitNewMsg(data, io, users) {
+    console.log(`${data.fromUsername} (${data.socketId}) says: ${data.msg}`);
+
+    let messageAttributes = {
+        content: data.msg,
+        fromUsername: data.fromUsername,
+        toUsername: data.toUsername
+    },
+    m = new Message(messageAttributes);
+
+    m.save()
+    .then(() => {
+        io.to(users[data.toUsername]).emit('to message', data);
+    })
+    .catch(error => console.log(`error: ${error.message}`));
+}
+
 // loads all messages (recent 10) 
 // sent to or received by this user
 function loadAllMessages(username, io, users) {
@@ -16,12 +35,24 @@ function loadAllMessages(username, io, users) {
         });
 }
 
-// message history between clinic user and a specific patient
+// message history between this user (me) and another specific user
 // e.g. only clinic 1 to pgangad, or pgangad to clinic1
-function messagesWithPatient(clinicUsername, patientUsername, io, users) {
-
+function messagesWithUser(myUsername, otherUsername, io, users) {
+    Message.find({
+        $or: [
+            { $and: [{fromUsername:myUsername},{toUsername:otherUsername}] }, 
+            { $and: [{fromUsername:otherUsername},{toUsername:myUsername}] }
+            ]
+        })
+        .sort({ createdAt: -1 })
+        .limit(10)
+        .then(messages => {
+            io.to(users[myUsername]).emit("load messages with user", messages.reverse());
+        });
 }
 
 module.exports = {
-    loadAllMessages
+    saveAndEmitNewMsg,
+    loadAllMessages,
+    messagesWithUser
 }
--- setupchat.js
+++ setupchat.js
@@ -1,7 +1,5 @@
 // convert a connect middleware to a Socket.IO middleware
 const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
-const Message = require('./models/message');
-const User = require('./models/user');
 const ChatController = require('./controllers/chat');
 
 function chatInit (io, sessionMiddleware, passport, users) {
@@ -32,44 +30,24 @@ function chatInit (io, sessionMiddleware, passport, users) {
         //map usernames to their socket ID's as key-value pairs
         users[socket.request.user.username] = socket.id;
 
-        /*
-        Message.find({
-            $or: [
-                { fromUsername: socket.request.user.username },
-                { toUsername: socket.request.user.username }
-                ]
-            })
-            .sort({ createdAt: -1 })
-            .limit(10)
-            .then(messages => {
-                io.to(users[socket.request.user.username]).emit("load all messages", messages.reverse());
-            });
-            */
         ChatController.loadAllMessages(socket.request.user.username, io, users);
-
 
         const session = socket.request.session;
         console.log(`saving sid ${socket.id} in session ${session.id}`);
         session.socketId = socket.id;
         session.save();
 
+        // save new messages to DB, and then
+        // emit the message to the recipient
         socket.on('from message', (data) => {    
-
-            console.log(`${data.fromUsername} (${data.socketId}) says: ${data.msg}`);
-
-            let messageAttributes = {
-                content: data.msg,
-                fromUsername: data.fromUsername,
-                toUsername: data.toUsername
-            },
-            m = new Message(messageAttributes);
-
-            m.save()
-            .then(() => {
-                io.to(users[data.toUsername]).emit('to message', data);
-            })
-            .catch(error => console.log(`error: ${error.message}`));
+           ChatController.saveAndEmitNewMsg(data, io, users);
         });
+
+        socket.on('target user', (data) => {
+            ChatController.messagesWithUser(socket.request.user.username, 
+                                            data.targetUsername, io, users);
+        });
+
     });
 }
 
