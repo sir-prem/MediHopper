9d30edc3b96281f01dea583bf4138ac906a14fd8
Prem G <sir_prem@hotmail.com>
2021-10-04 02:32:27 +1100

    Modified signup function, if new user is Clinic Admin Role, save new User to DB and new Clinic to DB.

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 var User = require("../models/user");
+var Clinic = require('../models/clinic');
 var fs = require("fs");
 
 function homepage(req, res, next) {
@@ -91,11 +92,26 @@ function signup (req, res, next) {
             mobile: mobile,
             email: email
         });
+
+        // if new user is a Clinic Admin role,
+        // then save the clinic to DB also
+        if (role === 'admin') {
+            var newClinic = new Clinic ({
+                username: username,
+                name: given,
+                address: `${street} ${city} ${state}`,
+                postcode: postcode,
+                phone: mobile
+            });
+            newClinic.save();
+        }
+
+        /*
         console.log("req.file is: " + req.file);
         console.log("req.file.filename is: " + req.file.filename);
         console.log("req.file.path is: " + req.file.path);
         console.log("req.file.fieldname is: " + req.file.fieldname);
-        //console.log("req.files[0].fieldname is: " + req.files[0].fieldname);
+        */
 
         newUser.profileImage.data = fs.readFileSync(req.file.path);
         newUser.profileImage.contentType = 'image/png';
