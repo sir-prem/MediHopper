8e21f9f570fb932efce7f74be34cbc11582a6126
Saleh <smalhar@deakin.edu.au>
2021-10-08 15:45:03 +1100

    Last changes before merge

--- routers/clinic.js
+++ routers/clinic.js
@@ -5,10 +5,11 @@ var ClinicController = require('../controllers/clinic');
 var Utils = require('../utils/utils');
 
 router.post("/search", Utils.ensureAuthenticated, ClinicController.search);
+router.post("/booking-confirmation", Utils.ensureAuthenticated, ClinicController.bookingConf);
+
+// adding queuing routes
 router.get("/clinics", Utils.ensureAuthenticated, ClinicController.showclinics);
 router.get("/clinicList/:clinicname", Utils.ensureAuthenticated, ClinicController.showclinicsPatients);
-
-router.post("/booking-confirmation", Utils.ensureAuthenticated, ClinicController.bookingConf);
 router.post("/remove-from-list", Utils.ensureAuthenticated, ClinicController.removeFromList);
 router.post("/make-last", Utils.ensureAuthenticated, ClinicController.makeLast);
 
--- utils/utils.js
+++ utils/utils.js
@@ -12,13 +12,13 @@ const User      = require("../models/user");
 // uses Passport.js's isAuthenticated() method
 // to confirm user authentication
 function ensureAuthenticated(req, res, next) {
-    next();
-    // if (req.isAuthenticated()) {
-    //     next();
-    // } else {
-    //     req.flash("info", "You must be logged in to see this page.");
-    //     res.redirect("/login");
-    // }
+    
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        req.flash("info", "You must be logged in to see this page.");
+        res.redirect("/login");
+    }
 }
 
 
